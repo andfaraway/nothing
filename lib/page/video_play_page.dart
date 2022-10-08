cc07d1aa7eecd8a6ee131b845befdd8d253586c2
libin <bin.li@imin.sg>
2022-10-08 10:16:11 +0800

    视频播放

--- lib/page/video_play_page.dart
+++ lib/page/video_play_page.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:nothing/page/video_screen.dart';
 import 'package:nothing/public.dart';
 import 'package:video_player/video_player.dart';
 import 'video_play_vm.dart';
@@ -12,9 +13,7 @@ class VideoPlayPage extends BasePage<_VideoPlayState> {
 }
 
 class _VideoPlayState extends BaseState<VideoPlayVM, VideoPlayPage> {
-  late VideoPlayerController _controller;
-  String url = 'http://1.14.252.115/something/1/1.mp4';
-  int quarterTurns = 0;
+  String url = 'http://1.14.252.115/something/1.mp4';
 
   @override
   VideoPlayVM createVM() => VideoPlayVM(context);
@@ -23,55 +22,6 @@ class _VideoPlayState extends BaseState<VideoPlayVM, VideoPlayPage> {
   void initState() {
     super.initState();
     pageTitle = "Video Play";
-    _controller = VideoPlayerController.network(url)
-      ..initialize().then((_) {
-        print('load success');
-        setState(() {});
-      }).catchError((error) {
-        print('load error:${error.toString()}');
-      });
-  }
-
-  @override
-  Widget? floatingActionButton() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        FloatingActionButton(
-          heroTag: '1',
-          onPressed: () {
-            setState(() {
-              _controller.value.isPlaying
-                  ? _controller.pause()
-                  : _controller.play();
-              pageTitle = null;
-              backgroundColor = Colors.black;
-              quarterTurns = 1;
-            });
-          },
-          child: Icon(
-            _controller.value.isPlaying ? Icons.pause : Icons.play_arrow,
-          ),
-        ),
-        const SizedBox(
-          height: 20,
-        ),
-        FloatingActionButton(
-          heroTag: '2',
-          onPressed: () {
-            _controller = VideoPlayerController.network(url)
-              ..initialize().then((_) {
-                print('load success');
-                setState(() {});
-              }).catchError((error) {
-                print('load error:${error.toString()}');
-              });
-            setState(() {});
-          },
-          child: const Icon(Icons.rotate_left),
-        ),
-      ],
-    );
   }
 
   @override
@@ -79,22 +29,22 @@ class _VideoPlayState extends BaseState<VideoPlayVM, VideoPlayPage> {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        if (!_controller.value.isPlaying)
-          TextField(
-            controller: TextEditingController(text: url),
-            onChanged: (value) {
-              url = value;
-            },
+        TextField(
+          controller: TextEditingController(text: url),
+          onChanged: (value) {
+            url = value;
+          },
+          textAlign: TextAlign.center,
+        ),
+        TextButton(
+          onPressed: () {
+            AppRoutes.pushPage(context, VideoScreen(url: url));
+          },
+          child: const Padding(
+            padding: EdgeInsets.all(30.0),
+            child: Text('next'),
           ),
-        _controller.value.isInitialized
-            ? RotatedBox(
-                quarterTurns: quarterTurns,
-                child: AspectRatio(
-                  aspectRatio: _controller.value.aspectRatio,
-                  child: VideoPlayer(_controller),
-                ),
-              )
-            : Container(),
+        ),
       ],
     );
   }
---
+++ lib/page/video_screen.dart
@@ -0,0 +1,64 @@
+import 'package:fijkplayer/fijkplayer.dart';
+import 'package:flutter/material.dart';
+
+class VideoScreen extends StatefulWidget {
+  final String url;
+
+  const VideoScreen({required this.url});
+
+  @override
+  _VideoScreenState createState() => _VideoScreenState();
+}
+
+class _VideoScreenState extends State<VideoScreen> {
+  final FijkPlayer player = FijkPlayer();
+  int quarterTurns = 0;
+
+  _VideoScreenState();
+
+  @override
+  void initState() {
+    super.initState();
+    player.setDataSource(widget.url, autoPlay: true);
+    player.addListener(() {
+      print('state = ${player.value.state}');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: quarterTurns != 0 ? null : AppBar(title: const Text("Video play")),
+      body: RotatedBox(
+        quarterTurns: quarterTurns,
+        child: Container(
+          alignment: Alignment.center,
+          child: FijkView(
+            fit: quarterTurns == 0 ? FijkFit.contain,
+            player: player,
+            color: Colors.black,
+          ),
+        ),
+      ),
+      floatingActionButton: IconButton(
+        onPressed: () {
+          if (quarterTurns < 3) {
+            quarterTurns += 1;
+          }else{
+            quarterTurns = 0;
+          }
+          setState(() {
+
+          });
+        },
+        icon: const Icon(Icons.rotate_right,color: Colors.red,),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
+  }
+}
